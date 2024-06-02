3d8492bf6ba8641470dcf114928f46af6da9fc73
Lakshya Kumar <kumarlakshya101@gmail.com>
2024-06-02 13:58:58 +0530

    feat: integrate apiError and apiResponse classes

--- src/index.js
+++ src/index.js
@@ -6,7 +6,19 @@ dotenv.config({
     path: "./env"
 })
 
-connectDB();
+connectDB()
+.then(() => {
+    app.on("ERROR", (error) => {
+        console.log("Error", error);
+        throw error
+    })
+    app.listen((process.env.PORT || 8000), () => {
+        console.log(`Server is running at port : ${process.env.PORT}`)
+    })
+})
+.catch((error) => [
+    console.log("MONGODB connection failed !!! ", error)
+])
 
 /*
 import express from "express"
