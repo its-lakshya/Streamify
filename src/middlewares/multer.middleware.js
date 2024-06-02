21abb3840270c4fa64357cb6d9554624748174c2
Lakshya Kumar <kumarlakshya101@gmail.com>
2024-06-02 22:48:52 +0530

    feat: integrate multer to upload files and encrypted the password

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,12 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/temp");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname + Math.floor(Math.random() * 6));
+  },
+});
+
+export const upload = multer({ storage });
