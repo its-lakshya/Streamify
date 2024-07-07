4536d69657fe0b118f2c3e9d171538988c451e80
Lakshya Kumar <kumarlakshya101@gmail.com>
2024-07-07 15:20:57 +0530

    feat: add toogleTweetLike method

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -5,6 +5,7 @@ import { apiError } from "../utils/apiError.js"
 import { apiResponse } from "../utils/apiresponse.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
 import { Comment } from "../models/comment.model.js"
+import { Tweet } from "../models/tweet.model.js"
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
@@ -92,6 +93,48 @@ const toggleCommentLike = asyncHandler(async (req, res) => {
 })
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
+  const { tweetId } = req.params;
+  const user = req.user;
+
+  if(!user){
+    throw new apiError(404, "Unauthorized request")
+  }
+
+  if(!mongoose.isValidObjectId(tweetId)){
+    throw new apiError(404, "Invalid tweet id");
+  }
+
+  const tweet = await Tweet.findById(tweetId);
+
+  const alreadyLiked = await Like.findOne({tweet, likedBy: user});
+
+  if(alreadyLiked){
+    const deleteLike = await Like.findByIdAndDelete(alreadyLiked._id);
+
+    if(!deleteLike){
+      throw new apiError(500, "Something went wrong while disliking the tweet")
+    }
+
+    return res
+      .status(200)
+      .json(new apiResponse(200, "Tweet disliked successfuly"));
+  }
+  else{
+    const like = await Like.create({
+      tweet: tweet,
+      likedBy: user
+    })
+  
+    const likedTweet = await Like.findById(like._id);
+  
+    if(!likedTweet){
+      throw new apiError(500, "Something went wrong while liking the tweet");
+    }
+  
+    return res
+      .status(200)
+      .json(new apiResponse(200, "Tweet liked successfuly"));
+  }
 
 })
 
