2ae868c2a0cf1348c44576dd47ec4aa14c388938
Lakshya Kumar <kumarlakshya101@gmail.com>
2024-07-08 00:28:08 +0530

    feat: add creatPlaylist method

--- src/app.js
+++ src/app.js
@@ -21,6 +21,7 @@ import commentRouter from "./routes/comment.routes.js";
 import likeRouter from "./routes/like.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
 
 // routes declaration
 app.use("/api/v1/users", userRouter);
@@ -29,6 +30,7 @@ app.use("/api/v1/comments", commentRouter);
 app.use("/api/v1/likes", likeRouter);
 app.use("/api/v1/tweets", tweetRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/playlist", playlistRouter)
 
 // http://localhost:8000/api/v1/user/register
 export { app };
---
+++ src/controllers/playlist.controller.js
@@ -0,0 +1,94 @@
+import mongoose from "mongoose";
+import { apiError } from "../utils/apiError.js";
+import { apiResponse } from "../utils/apiresponse.js";
+import { Playlist } from "../models/playlist.model.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { User } from "../models/user.model.js";
+import { Video } from "../models/video.model.js"
+
+const createPlaylist = asyncHandler(async (req, res) => {
+  const { name, description, videos } = req.body;
+  const user = req.user;
+
+  if(!user){
+    throw new apiError(404, "Unauthorized request please login to create playlist")
+  }
+
+  if(!name){
+    throw new apiError(404, "playlist name is required")
+  }
+
+  if(!description){
+    throw new apiError(404, "playlist description is required")
+  }
+
+  const newPlaylist = await Playlist.create({
+    name,
+    description,
+    owner: user,
+    videos,
+  })
+
+  const createdPlaylist = await Playlist.findById(newPlaylist._id);
+
+  if(!createdPlaylist){
+    throw new apiError(500, "Something went wrong while creating playlist")
+  }
+
+  return res
+    .status(200)
+    .json(new apiResponse(200, createdPlaylist, "Playlist created succussfully"))
+
+});
+
+const getPlaylistById = asyncHandler(async (req, res) => {
+  
+
+});
+
+const updatePlaylist = asyncHandler(async (req, res) => {
+  const { name, description } = req.body;
+  const { playlistId } = req.params;
+
+  if(!mongoose.isValidObjectId(playlistId)){
+    throw new apiError(404, "Invalid playlist id")
+  }
+
+  if(!name){
+    throw new apiError(404, "playlist name is required")
+  }
+
+  if(!description){
+    throw new apiError(404, "playlist description is required")
+  }
+
+
+
+});
+
+const deletePlaylist = asyncHandler(async (req, res) => {
+  
+
+});
+
+const addVideoToPlaylist = asyncHandler(async (req, res) => {
+  
+  
+});
+
+const removeVideoFromPlaylist = asyncHandler(async (req, res) => {});
+
+const getUserPlaylists = asyncHandler(async (req, res) => {
+  
+
+});
+
+export {
+  createPlaylist,
+  getPlaylistById,
+  updatePlaylist,
+  deletePlaylist,
+  addVideoToPlaylist,
+  removeVideoFromPlaylist,
+  getUserPlaylists,
+};
