757cfd626a5f1c776eb91363b24697d696acb8fb
Lakshya Kumar <kumarlakshya101@gmail.com>
2024-07-08 16:39:42 +0530

    feat: add getChannelStats method

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -8,8 +8,70 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { User } from "../models/user.model.js";
 
 const getChannelStats = asyncHandler(async (req, res) => {
-  
+  const user = req.user;
 
+  if(!user){
+    throw new apiError(404, "Unauthorized request");
+  }
+
+  const videos = await Video.find({owner: user});
+  const totalVideos = videos.length;
+
+  if(!totalVideos){
+    throw new apiError(500, "Something went wrong while fetching total videos")
+  }
+
+  const subscribers = await Subscription.find({channel: user});
+
+  if(!subscribers){
+    throw new apiError(500, "Something went wrong while fetching subscribers")
+  }
+
+  const totalSubscribers = subscribers.length;
+
+  const videoLikes = await Like.aggregate([
+    {
+      $match: {
+        video: {$ne: null},
+        comment: {$exists: false},
+        tweet: {$exists: false}
+      }
+    },
+    {
+      $lookup: {
+        from: 'videos',
+        localField: 'video',
+        foreignField: '_id',
+        as: 'likedVideoOwner',
+        pipeline: [
+          {
+            $project: {
+              owner: 1
+            }
+          }
+        ]
+      }
+    },
+    {
+      $unwind: '$likedVideoOwner'  // Assuming there's exactly one owner for each liked video
+    },
+    {
+      $match: {
+        'likedVideoOwner.owner': user._id
+      }
+    }
+  ])
+
+  if(!videoLikes){
+    throw new apiError(500, "Something went wrong while fetching likes")
+  }
+
+  const totalLikes = videoLikes.length;
+
+
+  return res
+    .status(200)
+    .json(new apiResponse(200, {totalVideos, totalSubscribers, totalLikes}, "Channel stats fetched successfully"))
 
 })
 
